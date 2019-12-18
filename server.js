221d0dbd41fbbdf60dc49d54bff6dd61d9db4038
RdotSilva <RyandotSilva@gmail.com>
2019-12-18 17:16:02 -0500

    Update server to connect to MongoDB

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDatabase = require("./config/db");
 
 // Load ENV variables
 dotenv.config({ path: "./config/config.env" });
+
+// Connect to DB
+connectDatabase();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections by logging error and closing server
+process.on("unhandledRejection", (error, promise) => {
+  console.log(`Error: ${error.message}`);
+  server.close(() => process.exit(1));
+});
