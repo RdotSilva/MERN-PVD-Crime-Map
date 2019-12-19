47ed9e62b68a86456694861d34a56c0d1ee8434b
RdotSilva <RyandotSilva@gmail.com>
2019-12-19 11:28:27 -0500

    Update register controller to create user

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,5 +5,14 @@ const User = require("../models/User");
 // @route   POST /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password
+  });
+
   res.status(200).json({ success: true });
 });
