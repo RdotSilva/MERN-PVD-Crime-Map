f2d5af709a9bae252ab99d73d3fedbdf0dfe3d3e
RdotSilva <RyandotSilva@gmail.com>
2019-12-19 11:32:05 -0500

    Add pre hook to hash password

--- Models/User.js
+++ Models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -24,6 +25,16 @@ const UserSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+});
+
+// Encrypt pw using bcrypt
+UserSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = mongoose.model("User", UserSchema);
