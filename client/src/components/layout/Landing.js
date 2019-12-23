a52ec0029bd99576e90bf47b65e9a8159b35a55c
RdotSilva <RyandotSilva@gmail.com>
2019-12-23 11:56:23 -0500

    Add Copyright component

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -8,6 +8,19 @@ import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Link from "@material-ui/core/Link";
 
+const Copyright = () => {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://rdotsilva.com">
+        RdotSilva
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+};
+
 const Landing = () => {
   const classes = useStyles();
   return (
