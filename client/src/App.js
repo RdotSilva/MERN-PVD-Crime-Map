385e1973d460177655056becb1e21844b912b7ba
RdotSilva <RyandotSilva@gmail.com>
2019-12-23 11:05:59 -0500

    Add Router to main App

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 
 const App = () => (
-  <Fragment>
-    <Navbar></Navbar>
-    <h1>PVD Crime Map</h1>
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar></Navbar>
+      <h1>PVD Crime Map</h1>
+    </Fragment>
+  </Router>
 );
 
 export default App;
