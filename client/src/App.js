6c484665c8b9d43c67c4fbf984b10a48fda4478c
RdotSilva <RyandotSilva@gmail.com>
2019-12-23 11:56:49 -0500

    Add Landing page to main App

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
 
 const App = () => (
   <Router>
     <Fragment>
       <Navbar></Navbar>
-      <h1>PVD Crime Map</h1>
+      <Route exact path="/" component={Landing}></Route>
     </Fragment>
   </Router>
 );
