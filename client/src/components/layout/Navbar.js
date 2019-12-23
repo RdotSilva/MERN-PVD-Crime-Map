4b73aea7ea1f85db2531901d1d5ea176335cf832
RdotSilva <RyandotSilva@gmail.com>
2019-12-23 10:57:31 -0500

    Add Navbar component

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+const Navbar = () => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <i className="fas fa-map-pin"></i>
+        <Typography variant="h6" className={classes.title}>
+          Crime Map
+        </Typography>
+        <Button color="inherit">Register</Button>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
