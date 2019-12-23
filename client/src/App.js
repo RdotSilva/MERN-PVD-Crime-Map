cf997f81daef3178a5d03dae1184ffbba0e617b5
RdotSilva <RyandotSilva@gmail.com>
2019-12-23 11:04:06 -0500

    Add Navbar to main App

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React, { Fragment } from "react";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
 
 const App = () => (
   <Fragment>
+    <Navbar></Navbar>
     <h1>PVD Crime Map</h1>
   </Fragment>
 );
