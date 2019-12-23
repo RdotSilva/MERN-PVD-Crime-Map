4eb3bd72b987937057a4bc86ee4fcc91d6cf10cb
RdotSilva <RyandotSilva@gmail.com>
2019-12-23 11:55:56 -0500

    Add Landing component

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,72 @@
+import React, { Fragment } from "react";
+
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+
+const Landing = () => {
+  const classes = useStyles();
+  return (
+    <Fragment>
+      <CssBaseline />
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              PVD Crime Map
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Stay up to date with recent crimes committed in your neighborhood.
+            </Typography>
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    Recent Crimes
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Crime Map
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+      </main>
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          PVD Crime Map
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
+          Stay Informed
+        </Typography>
+        <Copyright />
+      </footer>
+    </Fragment>
+  );
+};
+
+export default Landing;
