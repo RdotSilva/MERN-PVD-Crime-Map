c1c3020bf61dbd32a6da840ea927bcc5642754a7
RdotSilva <RyandotSilva@gmail.com>
2019-12-24 17:22:59 -0500

    Add Register link to Navbar with router

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import Link from "@material-ui/core/Link";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -25,8 +27,12 @@ const Navbar = () => {
         <Typography variant="h6" className={classes.title}>
           Crime Map
         </Typography>
-        <Button color="inherit">Register</Button>
-        <Button color="inherit">Login</Button>
+        <Button color="inherit">
+          <Link color="inherit" component={RouterLink} to="/register">
+            Register
+          </Link>
+        </Button>
+        <Button color="inherit"></Button>
       </Toolbar>
     </AppBar>
   );
