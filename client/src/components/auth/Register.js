c1e53bd81cea5e80c83efa8ccca59cf5ee335b49
RdotSilva <RyandotSilva@gmail.com>
2019-12-25 20:01:48 -0500

    Add formData state

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,6 +3,15 @@ import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: ""
+  });
+
+  const { name, email, password, password2 } = formData;
+
   return <div>Register</div>;
 };
 
