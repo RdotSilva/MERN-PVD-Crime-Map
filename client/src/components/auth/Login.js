9269fe377c8a3ed6d317b229e0db3876b6b5f940
RdotSilva <RyandotSilva@gmail.com>
2019-12-26 14:25:41 -0500

    Add useStyles for Login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -4,8 +4,19 @@ import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Icon from "@material-ui/core/Icon";
 
-const Login = () => {
-  return <div></div>;
-};
+// Styles used for Material UI
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: 200
+    },
+    button: {
+      margin: theme.spacing(1)
+    }
+  }
+}));
+
+const Login = () => {};
 
 export default Login;
