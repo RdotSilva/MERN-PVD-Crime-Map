ff4ac76a0f62e8174153f711ea435e055aa70bf1
RdotSilva <RyandotSilva@gmail.com>
2019-12-26 14:24:46 -0500

    Add Login component

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,11 @@
+import React, { Fragment, useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Icon from "@material-ui/core/Icon";
+
+const Login = () => {
+  return <div></div>;
+};
+
+export default Login;
