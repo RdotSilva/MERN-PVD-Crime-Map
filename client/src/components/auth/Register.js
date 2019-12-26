a69c5f98b46abb8b08c03480cb9fc20b0b7865a9
RdotSilva <RyandotSilva@gmail.com>
2019-12-25 20:13:35 -0500

    Add submit button to form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,8 @@
 import React, { Fragment, useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Icon from "@material-ui/core/Icon";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -87,6 +89,15 @@ const Register = () => {
           required
         />
       </div>
+      <Button
+        variant="contained"
+        color="primary"
+        className={classes.button}
+        endIcon={<Icon>send</Icon>}
+        type="submit"
+      >
+        Send
+      </Button>
     </form>
   );
 };
