aaaf893c6d7338a065fb141b10497e054e983a5c
RdotSilva <RyandotSilva@gmail.com>
2019-12-25 19:59:42 -0500

    Add Register component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,1 +1,9 @@
-d;
+import React, { Fragment, useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+
+const Register = () => {
+  return <div>Register</div>;
+};
+
+export default Register;
