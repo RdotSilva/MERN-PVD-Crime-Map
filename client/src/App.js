bd1f0ce03e8cc38b4e18dd8966d91e13081af878
RdotSilva <RyandotSilva@gmail.com>
2019-12-25 19:43:07 -0500

    Add Register route to main App

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,16 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
 
 const App = () => (
   <Router>
     <Fragment>
       <Navbar></Navbar>
       <Route exact path="/" component={Landing}></Route>
+      <Switch>
+        <Route exact path="/register" component={Register} />
+      </Switch>
     </Fragment>
   </Router>
 );
