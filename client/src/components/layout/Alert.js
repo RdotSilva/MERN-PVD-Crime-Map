92cd1761983b9681f6c8af13c0e0a639fa2b208a
RdotSilva <RyandotSilva@gmail.com>
2019-12-30 13:08:39 -0500

    Add temp fix for Alert crash

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -3,7 +3,8 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 const Alert = ({ alerts }) =>
-  alerts !== null &&
+  // Temp change from null to undefined to prevent crash
+  alerts !== undefined &&
   alerts.length > 0 &&
   alerts.map(alert => (
     <div key={alert.id} className={`alert alert-${alert.alertType}`}>
