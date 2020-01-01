e1c38366ece030f0e607fcd37fc0189beb706c44
RdotSilva <RyandotSilva@gmail.com>
2020-01-01 11:55:29 -0500

    Add TODO in try/catch

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
-import { SET_ALERT } from "./alert";
+import { setAlert } from "./alert";
 
 // Register User
 export const register = ({ name, email, password }) => async dispatch => {
@@ -13,7 +13,8 @@ export const register = ({ name, email, password }) => async dispatch => {
   const body = JSON.stringify({ name, email, password });
 
   try {
-    const res = await axios.post("/api/users", body, config);
+    // TODO fix this response. Coming up with 500 error.
+    const res = await axios.post("/api/v1/auth/register", body, config);
     dispatch({
       type: REGISTER_SUCCESS,
       payload: res.data
