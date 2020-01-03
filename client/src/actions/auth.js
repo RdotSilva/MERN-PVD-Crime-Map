744f1c73869e8d83ce4df3de94030d46274f41d0
RdotSilva <RyandotSilva@gmail.com>
2020-01-03 13:39:49 -0500

    Add login action

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
   USER_LOADED,
   AUTH_ERROR
 } from "./types";
@@ -39,7 +41,6 @@ export const register = ({ name, email, password }) => async dispatch => {
   const body = JSON.stringify({ name, email, password });
 
   try {
-    // TODO fix this response. Coming up with 500 error.
     const res = await axios.post("/api/v1/auth/register", body, config);
     dispatch({
       type: REGISTER_SUCCESS,
@@ -54,6 +55,35 @@ export const register = ({ name, email, password }) => async dispatch => {
 
     dispatch({
       type: REGISTER_FAIL
+    });
+  }
+};
+
+// Login User
+export const login = (email, password) => async dispatch => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post("/api/v1/auth/login", body, config);
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    });
+  } catch (error) {
+    const errors = error.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
+
+    dispatch({
+      type: LOGIN_FAIL
     });
   }
 };
