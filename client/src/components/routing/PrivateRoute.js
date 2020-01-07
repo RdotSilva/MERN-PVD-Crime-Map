fbff4cf59d6ac6f91f1c77f8df8431aea4131546
RdotSilva <RyandotSilva@gmail.com>
2020-01-06 19:23:45 -0500

    Fix Redux hooks

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -4,20 +4,22 @@ import { Route, Redirect } from "react-router-dom";
 // Redux
 import { useSelector } from "react-redux";
 
-const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
-const isLoading = useSelector(state => state.auth.isLoading);
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+  const isLoading = useSelector(state => state.auth.isLoading);
 
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={props =>
-      !isAuthenticated && !isLoading ? (
-        <Redirect to="/login" />
-      ) : (
-        <Component {...props} />
-      )
-    }
-  />
-);
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        !isAuthenticated && !isLoading ? (
+          <Redirect to="/login" />
+        ) : (
+          <Component {...props} />
+        )
+      }
+    />
+  );
+};
 
 export default PrivateRoute;
