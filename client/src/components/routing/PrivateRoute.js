2083d0560199a3eed735667a2aa11b23a2415396
RdotSilva <RyandotSilva@gmail.com>
2020-01-06 19:15:13 -0500

    Add redux state hooks

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+// Redux
+import { useSelector } from "react-redux";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  return <div></div>;
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+  const isLoading = useSelector(state => state.auth.isLoading);
+
+  <Route
+    {...rest}
+    render={props =>
+      !isAuthenticated && !isLoading ? (
+        <Redirect to="/login" />
+      ) : (
+        <Component {...props} />
+      )
+    }
+  />;
 };
 
 export default PrivateRoute;
