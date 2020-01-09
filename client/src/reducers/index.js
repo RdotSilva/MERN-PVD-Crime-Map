ebdd910476cf94740edc9ecedc346c4d6f4d0b6f
RdotSilva <RyandotSilva@gmail.com>
2020-01-09 15:24:46 -0500

    Add crime to combineReducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import alert from "./alert";
 import auth from "./auth";
+import crime from "./crime";
 
-export default combineReducers({ alert, auth });
+export default combineReducers({ alert, auth, crime });
