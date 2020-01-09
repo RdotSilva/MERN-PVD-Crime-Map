4b220777b005a02ebcadf14b3b98d64ab88bc346
RdotSilva <RyandotSilva@gmail.com>
2020-01-08 19:32:51 -0500

    Add header & description for Dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,12 +1,71 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+import Copyright from "./../layout/Copyright";
 
 // Redux
 import { useSelector } from "react-redux";
 
 const Dashboard = () => {
-  const userData = useSelector(state => state.auth.user);
-  const userName = userData.data.name;
-  return <div>Welcome {userName}</div>;
+  return (
+    <Fragment>
+      <CssBaseline />
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              PVD Crime Map
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Stay up to date with recent crimes committed in your neighborhood.
+            </Typography>
+            <Typography
+              variant="subtitle2"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Select "Crime List" to view a list of recent crimes in Providence.
+              Select "Crime Map" to see a map view of recent crimes in
+              Providence.
+            </Typography>
+          </Container>
+        </div>
+      </main>
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          PVD Crime Map
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
+          Stay Informed
+        </Typography>
+        <Copyright />
+      </footer>
+    </Fragment>
+  );
 };
 
 export default Dashboard;
