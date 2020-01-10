11c58dda8555afa11acf14ff39422e889d67e67d
RdotSilva <RyandotSilva@gmail.com>
2020-01-10 15:06:00 -0500

    Add Redux dispatch & actions

--- client/src/components/crime/CrimeList.js
+++ client/src/components/crime/CrimeList.js
@@ -1,6 +1,12 @@
 import React, { useEffect } from "react";
 
+// Redux
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCrimeData } from "./../../actions/crime";
+
 const CrimeList = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(state => state.crime.loading);
   return <div></div>;
 };
 
