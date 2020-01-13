d25e1f23c056b4866d37b8c315555bdb3bbaf311
RdotSilva <RyandotSilva@gmail.com>
2020-01-13 09:41:01 -0500

    Add google maps options

--- client/src/components/crime/CrimeMap.js
+++ client/src/components/crime/CrimeMap.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { GoogleMap, useLoadScript } from '@react-google-maps/api'
+
+const options = {
+  zoomControlOptions: {
+    position: google.maps.ControlPosition.RIGHT_CENTER
+  }
+}
 
 const CrimeMap = () => {
+  
+
   return ()
     
 };
