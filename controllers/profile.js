512ad619fb696d0dafaba36017ba329107e69844
RdotSilva <RyandotSilva@gmail.com>
2020-01-15 16:57:06 -0500

    Add createdOrUpdateUserProfile controller

--- controllers/profile.js
+++ controllers/profile.js
@@ -6,11 +6,35 @@ const ErrorResponse = require("../utils/errorResponse");
 // @route   GET /api/v1/profile/me
 // @access  Private
 exports.getLoggedInUserProfile = asyncHandler(async (req, res, next) => {
-  const profile = await await Profile.findOne({ user: req.user.id });
+  const profile = await Profile.findOne({ user: req.user.id });
 
   if (!profile) {
     return next(new ErrorResponse("No profile data found", 400));
   }
+
+  res.status(200).json({
+    success: true,
+    data: profile
+  });
+});
+
+// @desc     Create or update user profile
+// @route    POST/api/v1/profile
+// @access   Private
+exports.createdOrUpdateUserProfile = asyncHandler(async (req, res, next) => {
+  const { address } = req.body;
+
+  // Build profile object
+  const profileFields = {};
+  profileFields.user = req.user.id;
+  if (address) profileFields.address = address;
+
+  // Using upsert option (creates new doc if no match is found):
+  let profile = await Profile.findOneAndUpdate(
+    { user: req.user.id },
+    { $set: profileFields },
+    { new: true, upsert: true }
+  );
 
   res.status(200).json({
     success: true,
