0ecfa41f8a86f3747ce1333801ebd669ee820f9f
RdotSilva <RyandotSilva@gmail.com>
2020-01-15 16:57:40 -0500

    Add create/update profile route

--- routes/profile.js
+++ routes/profile.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const { getLoggedInUserProfile } = require("../controllers/profile");
+const {
+  getLoggedInUserProfile,
+  createdOrUpdateUserProfile
+} = require("../controllers/profile");
 
 const router = express.Router();
 
 const { protect } = require("../middleware/auth");
 
 router.get("/me", protect, getLoggedInUserProfile);
+router.post("/", protect, createdOrUpdateUserProfile);
 
 module.exports = router;
