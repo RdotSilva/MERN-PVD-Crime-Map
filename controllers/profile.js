47484e2f1fbddebb8a3bb8a15c2923723b09f328
RdotSilva <RyandotSilva@gmail.com>
2020-01-15 16:29:32 -0500

    Add getLoggedInUserProfile controller

---
+++ controllers/profile.js
@@ -0,0 +1,21 @@
+const asyncHandler = require("../middleware/async");
+const Profile = require("../models/Profile");
+const ErrorResponse = require("../utils/errorResponse");
+
+// @desc    Get current users profile
+// @route   GET /api/v1/profile/me
+// @access  Private
+exports.getLoggedInUserProfile = asyncHandler(async (req, res, next) => {
+  const profile = await (
+    await Profile.findOne({ user: req.user.id })
+  ).populate("user", ["name"]);
+
+  if (!profile) {
+    return next(new ErrorResponse("No profile data found", 400));
+  }
+
+  res.status(200).json({
+    success: true,
+    data: profile
+  });
+});
