5d8b5b8d9c81e0139254614f73277b9f9968d680
RdotSilva <RyandotSilva@gmail.com>
2020-01-17 17:35:16 -0500

    Add profile to combineReducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,5 +2,6 @@ import { combineReducers } from "redux";
 import alert from "./alert";
 import auth from "./auth";
 import crime from "./crime";
+import profile from "./profile";
 
-export default combineReducers({ alert, auth, crime });
+export default combineReducers({ alert, auth, crime, profile });
