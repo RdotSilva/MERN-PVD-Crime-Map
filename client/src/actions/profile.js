d80380bdc8508e87a4f327d4a6c5a9665e5a99ce
RdotSilva <RyandotSilva@gmail.com>
2020-01-17 18:38:14 -0500

    Remove debugging console logs

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -9,17 +9,14 @@ import { setAlert } from "./alert";
 
 // Get Current User Profile
 export const getCurrentUserProfile = () => async dispatch => {
-  console.log(`Fetching User Profile Data`);
   try {
     const res = await axios.get("/api/v1/profile/me");
-    console.log(`User Profile res.data = : ${res.data}`);
 
     dispatch({
       type: GET_PROFILE,
       payload: res.data
     });
   } catch (error) {
-    console.log(`Fetch User Profile Data Error: ${error}`);
     dispatch({
       type: PROFILE_ERROR,
       payload: { msg: error.response.statusText, status: error.response.status }
