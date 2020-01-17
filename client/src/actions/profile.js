be804aa778596632ba6b130f544d300a3495bc9b
RdotSilva <RyandotSilva@gmail.com>
2020-01-17 17:33:57 -0500

    Add getCurrentUserProfile action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,3 +1,28 @@
 import axios from "axios";
-import { GET_PROFILE, UPDATE_PROFILE, CLEAR_PROFILE } from "./types";
+import {
+  GET_PROFILE,
+  UPDATE_PROFILE,
+  CLEAR_PROFILE,
+  PROFILE_ERROR
+} from "./types";
 import { setAlert } from "./alert";
+
+// Get Current User Profile
+export const getCurrentUserProfile = () => async dispatch => {
+  console.log(`Fetching User Profile Data`);
+  try {
+    const res = await axios.get("/api/v1/profile/me");
+    console.log(`User Profile res.data = : ${res.data}`);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (error) {
+    console.log(`Fetch User Profile Data Error: ${error}`);
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
