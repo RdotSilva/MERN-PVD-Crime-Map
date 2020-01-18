598171cf5c15e675bbba66f1838abb3cb71c126f
RdotSilva <RyandotSilva@gmail.com>
2020-01-18 17:33:16 -0500

    Add useStyles for Material UI

--- client/src/components/profile/CreateProfile.js
+++ client/src/components/profile/CreateProfile.js
@@ -1,9 +1,33 @@
 import React, { useEffect, useState, Fragment } from "react";
 import { Link, withRouter, Redirect } from "react-router-dom";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Icon from "@material-ui/core/Icon";
 
 // Redux
 import { useDispatch, useSelector } from "react-redux";
 import { getCurrentUserProfile } from "./../../actions/profile";
+
+// Styles used for Material UI
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: 200
+    },
+    button: {
+      margin: theme.spacing(1)
+    }
+  },
+  center: {
+    textAlign: "center"
+  },
+  signUp: {
+    textAlign: "center",
+    padding: "10px"
+  }
+}));
 
 const CreateProfile = () => {
   const dispatch = useDispatch();
