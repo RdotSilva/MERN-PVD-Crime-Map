42a0b8fa71ef61aadb7fa782122ac9342b3c5e37
RdotSilva <RyandotSilva@gmail.com>
2020-01-18 17:04:06 -0500

    Add redux state for profile

--- client/src/components/profile/CreateProfile.js
+++ client/src/components/profile/CreateProfile.js
@@ -1,9 +1,17 @@
 import React, { useEffect, useState, Fragment } from "react";
 
+// Redux
+import { useDispatch, useSelector } from "react-redux";
+import { getCurrentUserProfile } from "./../../actions/profile";
+
 const CreateProfile = () => {
   const [formData, setFormData] = useState({
     address: ""
   });
+
+  // User Profile data from profile state
+  const isLoading = useSelector(state => state.profile.loading);
+  const profile = useSelector(state => state.profile.profile);
 
   const { address } = formData;
 
