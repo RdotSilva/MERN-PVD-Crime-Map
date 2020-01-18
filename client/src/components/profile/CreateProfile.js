6f5eee3c1de3077a06fb0c4c878d9fd72b43835f
RdotSilva <RyandotSilva@gmail.com>
2020-01-18 17:03:36 -0500

    Add formData state

--- client/src/components/profile/CreateProfile.js
+++ client/src/components/profile/CreateProfile.js
@@ -1,6 +1,12 @@
 import React, { useEffect, useState, Fragment } from "react";
 
 const CreateProfile = () => {
+  const [formData, setFormData] = useState({
+    address: ""
+  });
+
+  const { address } = formData;
+
   return <div></div>;
 };
 
