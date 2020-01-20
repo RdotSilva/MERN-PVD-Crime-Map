c452b80880c71bdd07976e1307e6e6531d4d338d
RdotSilva <RyandotSilva@gmail.com>
2020-01-19 19:11:56 -0500

    Add redux crime  state

--- client/src/components/crime/CrimeMap.js
+++ client/src/components/crime/CrimeMap.js
@@ -1,16 +1,21 @@
-import React, { Fragment } from "react";
+import React, { useEffect, Fragment } from "react";
 import { LoadScript, GoogleMap, Marker } from "@react-google-maps/api";
-
 // Redux
 import { useDispatch, useSelector } from "react-redux";
+import { fetchCrimeData } from "./../../actions/crime";
 
 const google = window.google;
-
 const apiKey = process.env.REACT_APP_API_KEY;
 
 const CrimeMap = () => {
+  const dispatch = useDispatch();
+
   // User Profile data from profile state
   const profile = useSelector(state => state.profile.profile);
+
+  // Crime Data from crime state
+  const isLoading = useSelector(state => state.crime.loading);
+  const crimes = useSelector(state => state.crime.crimes);
 
   // User Home Position set in profile data
   // Also use this to center map
@@ -21,19 +26,23 @@ const CrimeMap = () => {
 
   return (
     <Fragment>
-      <LoadScript id="script-loader" googleMapsApiKey={apiKey}>
-        <GoogleMap
-          id="crime-map"
-          mapContainerStyle={{
-            height: "900px",
-            width: "1900px"
-          }}
-          zoom={15}
-          center={position}
-        >
-          <Marker position={position} title="Home"></Marker>
-        </GoogleMap>
-      </LoadScript>
+      {isLoading ? (
+        <div>Loading</div>
+      ) : (
+        <LoadScript id="script-loader" googleMapsApiKey={apiKey}>
+          <GoogleMap
+            id="crime-map"
+            mapContainerStyle={{
+              height: "900px",
+              width: "1900px"
+            }}
+            zoom={15}
+            center={position}
+          >
+            <Marker position={position} title="Home"></Marker>
+          </GoogleMap>
+        </LoadScript>
+      )}
     </Fragment>
   );
 };
