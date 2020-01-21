8be0da5d17711c002fffe30b1fe72004c0316c63
RdotSilva <RyandotSilva@gmail.com>
2020-01-20 20:53:10 -0500

    Add react-geocode settings

--- client/src/components/crime/CrimeMap.js
+++ client/src/components/crime/CrimeMap.js
@@ -1,11 +1,24 @@
 import React, { useEffect, Fragment } from "react";
-import { LoadScript, GoogleMap, Marker } from "@react-google-maps/api";
+import {
+  LoadScript,
+  GoogleMap,
+  Marker,
+  MarkerClusterer
+} from "@react-google-maps/api";
+
+//GEOCODE
+import Geocode from "react-geocode";
+
 // Redux
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCrimeData } from "./../../actions/crime";
 
 const google = window.google;
 const apiKey = process.env.REACT_APP_API_KEY;
+
+// GEOCODE SETTINGS
+Geocode.setApiKey(apiKey);
+Geocode.enableDebug();
 
 const CrimeMap = () => {
   const dispatch = useDispatch();
@@ -28,19 +41,6 @@ const CrimeMap = () => {
     dispatch(fetchCrimeData());
   }, [dispatch]);
 
-  const geocodeCrime = async crime => {
-    // TODO: Add react geocoder and update this
-    const loc = await geocoder.geocode(crime.location);
-
-    const location = {
-      type: "Point",
-      coordinates: [loc[0].longitude, loc[0].latitude],
-      formattedAddress: loc[0].formattedAddress
-    };
-
-    return <Marker position={location.coordinates}></Marker>;
-  };
-
   return (
     <Fragment>
       {isLoading ? (
@@ -57,7 +57,6 @@ const CrimeMap = () => {
             center={position}
           >
             <Marker position={position} title="Home"></Marker>
-            {crimes.map(crime => geocodeCrime(crime))}
           </GoogleMap>
         </LoadScript>
       )}
